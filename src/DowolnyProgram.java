ba983f774552764151bee539371215bc637aad23
Brumzgar <gamrackiwojciech@wp.pl>
2021-01-06 17:54:59 +0100

    Zmiany

--- src/DowolnyProgram.java
+++ src/DowolnyProgram.java
@@ -1,5 +1,7 @@
 public class DowolnyProgram {
     public static void main(String[] args) {
         System.out.println("Dowolny Program");
+        System.out.println("Uaktualnij projekt");
+        //dowolny komentarz
     }
 }
